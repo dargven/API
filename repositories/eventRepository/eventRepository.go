30cd9134a2e6dce9ef148d1af41a36e4ca688e07
EgorLukyanovich <Elikanovich@gmail.com>
2024-12-13 13:49:16 +0400

    validate for Event Rep

--- repositories/eventRepository/eventRepository.go
+++ repositories/eventRepository/eventRepository.go
@@ -47,40 +47,80 @@ func (h *EventRep) EventMigrations() error {
 
 }
 
+func (h *EventRep) IsTitleUnique(event event.Event) (bool, error) {
+	query := `SELECT NOT EXISTS (SELECT 1 FROM events WHERE title = $1)`
+	var isUnique bool
+	err := h.h.Pool.QueryRow(context.Background(), query, event.Title).Scan(&isUnique)
+	if err != nil {
+		h.logger.Error("failed to check title uniqueness ", sl.Err(err))
+		return false, fmt.Errorf(" failed to check title uniqueness: %w", err)
+	}
+
+	return isUnique, nil
+}
+
+func (h *EventRep) IsEventExist(event event.Event) (bool, error) {
+	query := `SELECT NOT EXISTS (SELECT 1 FROM events WHERE id = $1)`
+	var isExist bool
+	err := h.h.Pool.QueryRow(context.Background(), query, event.Title).Scan(&isExist)
+	if err != nil {
+		h.logger.Error("failed to check existion of this event ", sl.Err(err))
+		return false, fmt.Errorf(" failed to check existion of this event:  %w", err)
+	}
+
+	return isExist, nil
+}
+
 func (h *EventRep) AddEvent(event event.Event) (int64, error) {
 	query := `INSERT INTO events (user_id, title, description, date, location, status, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
-	err := h.h.Pool.QueryRow(context.Background(), query, event.UserId, event.Title, event.Description, event.Date, event.Location, event.Status, event.CreatedAt).Scan(&event.ID)
-	if err != nil {
-		h.logger.Error("failed to create event")
 
-		return 0, fmt.Errorf("failed to create event: %w", err)
+	unique, err := h.IsTitleUnique(event)
+
+	if err != nil {
+		return 0, fmt.Errorf("failed to check title uniqueness: %w", err)
+	}
+
+	if unique {
+
+		err := h.h.Pool.QueryRow(context.Background(), query, event.UserId, event.Title, event.Description, event.Date, event.Location, event.Status, event.CreatedAt).Scan(&event.ID)
+		if err != nil {
+			h.logger.Error("failed to create event")
+
+			return 0, fmt.Errorf("failed to create event: %w", err)
+		}
+
+	} else if !unique {
+		h.logger.Error("event title is not unique")
+
+		return 0, fmt.Errorf("event title is not unique")
 	}
 
 	return event.ID, nil
 }
 
-func (h *EventRep) GetEvent(ID int) ([]event.Event, error) {
+func (h *EventRep) GetEvent(id int) (event.Event, error) {
 	query := `SELECT id, user_id, title, description, date, location, status, created_at FROM events WHERE id = $1;`
 
-	rows, err := h.h.Pool.Query(context.Background(), query, ID)
+	var ev event.Event
+
+	exist, err := h.IsEventExist(ev)
+
 	if err != nil {
-		return nil, fmt.Errorf("failed to get events: %w", err)
+		return ev, fmt.Errorf("failed to check title uniqueness: %w", err)
 	}
-	defer rows.Close()
 
-	var events []event.Event
-	for rows.Next() {
-		var ev event.Event
-		if err := rows.Scan(&ev.ID, &ev.UserId, &ev.Title, &ev.Description, &ev.Date, &ev.Location, &ev.Status, &ev.CreatedAt); err != nil {
-			return nil, fmt.Errorf("failed to scan event: %w", err)
+	if exist {
+		err := h.h.Pool.QueryRow(context.Background(), query, id).Scan(&ev.ID, &ev.UserId, &ev.Title, &ev.Description, &ev.Date, &ev.Location, &ev.Status, &ev.CreatedAt)
+		if err != nil {
+			h.logger.Error("event with ID not found: ", id, " || ", sl.Err(err))
+			return ev, fmt.Errorf("event with ID %d not found", id)
 		}
-		events = append(events, ev)
+	} else if !exist {
+		h.logger.Error("event with that id is not exist", sl.Err(err))
+		return ev, fmt.Errorf("event with that id is not exist", err)
 	}
 
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("rows iteration error: %w", err)
-	}
-	return events, nil
+	return ev, nil
 }
 
 func (h *EventRep) GetAllEvents() ([]event.Event, error) {
